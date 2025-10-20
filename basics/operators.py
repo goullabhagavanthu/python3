c9107789bc94e37493614e84c0db9d86dad4badd
goullabhagavanthu <goullabhagavanthu@gmail.com>
2025-10-20 06:10:53 +0000

    data types

---
+++ basics/operators.py
@@ -0,0 +1,71 @@
+#>>> 4 - 5  
+#-1
+#>>> 24 / 5
+#4.8
+#>>> 4 // 20
+#0
+#>>> rount(24  / 5)
+#Traceback (most recent call last):
+ # File "<stdin>", line 1, in <module>
+#NameError: name 'rount' is not defined. Did you mean: 'round'?
+#>>> round(24 / 5)
+#5
+#>>> 2 **5
+#32
+#>>> meaning = 42
+#>>> meaning += 1
+#>>> meaning
+#43
+#>>> meaning -= 1
+#>>> meaning
+#42
+#>>> meaning *= 10
+#>>> meaning
+#420
+#>>> meaning /= 10
+#>>> meaning
+#42.0
+#>>> round(meaning)
+#42
+#>>> meaning = round(meaning)
+#>>> meaning
+#42
+#>>> "goulla" + "bhagavanthu"
+#'goullabhagavanthu'
+#>>> "goulla " + "bhagavanthu"
+#'goulla bhagavanthu'
+#>>> 42 == 42
+#True
+#>>> 42 == 41
+#False
+#>>> 42 != 42
+#False
+#>>> 43 != 43
+#False
+#>>> 10 > 5
+#True
+#>>> 10 <5 
+#False
+#>>> 10 >= 10
+#True
+#>>> 10 <= 10
+#True
+#>>> x = true
+#Traceback (most recent call last):
+ # File "<stdin>", line 1, in <module>
+#NameError: name 'true' is not defined. Did you mean: 'True'?
+#>>> x = True
+#>>> y = False
+#>>> z = True
+#>>> not x
+#False
+#>>> not y
+#True
+#>>> x and y
+#False
+#>>> y and x
+#False
+#>>> x or y
+#True
+#>>> y or x
+#True
---
+++ data_types/types.py
@@ -0,0 +1,28 @@
+# literal assignments
+first = "goulla"
+last =  "bhagavanthu"
+
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+# constructor function
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))   
+
+# Concatenation
+fullname = first + " " + last
+print(fullname)
+
+fullname += "!"
+print(fullname)
+
+# Casting a number to string
+decade = str(1960)
+print(type(decade))
+print(decade)
+
+statement = "I like the music from the " + decade + "s."
+print(statement)
